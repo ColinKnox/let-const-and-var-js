c7fd524805589b94c704bba0b24de10693d736ea
ColinKnox <colinwknox.com>
2024-11-02 20:45:09 -0400

    Added starter code.

---
+++ script.js
@@ -0,0 +1,8 @@
+const district = "7th District";
+const city = "Boston";
+const usStateRepresentative = "Ayanna Pressley";
+const isLargestCity = true;
+const population = 667120;
+let language = "English";
+
+console.log(language);
